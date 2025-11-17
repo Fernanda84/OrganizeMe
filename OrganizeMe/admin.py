7cd11d7f76817e1b1b134fd1ef8d3b76e356c52d
Fernanda84 <fernandaliliam9@gmail.com>
2025-11-17 15:18:21 -0300

    deletando

--- OrganizeMe/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Atividade, Perfil, UserCostumizado
-
-
-admin.site.register(UserCostumizado)
-admin.site.register(Atividade)
-admin.site.register(Perfil)
-
